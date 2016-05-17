65ca93e046993910995b134986d43aaee14261c6
Ron Compos <rcompos@gmail.com>
2016-05-17 09:39:38 -0600

    Changed quotes.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,20 +1,20 @@
 class oraaud::install (
 ) inherits oraaud {
 
-   case $::osfamily {
-     'RedHat': {
-        if $::operatingsystemmajrelease + 0 >= 8 {
-          fail("Class['oraaud::install']: Unsupported operating system majrelease ${::operatingsystemmajrelease}")
-        }
-      }
-      default: {
-         fail("Class['oraaud::install']: Unsupported osfamily: ${::osfamily}")
-       }
-   }
+  #   case $::osfamily {
+  #   'RedHat': {
+  #      if $::operatingsystemmajrelease + 0 >= 8 {
+  #        fail("Class['oraaud::install']: Unsupported operating system majrelease ${::operatingsystemmajrelease}")
+  #      }
+  #    }
+  #    default: {
+  #       fail("Class['oraaud::install']: Unsupported osfamily: ${::osfamily}")
+  #     }
+  # }
 
-   package { '$expect_package':
-     ensure => $expect_ensure,
-     name   => $expect_package,
+   package { "$expect_package":
+     ensure => "$expect_ensure",
+     name   => "$expect_package",
    }
 
    staging::deploy { "$file_tar":
@@ -43,7 +43,7 @@ class oraaud::install (
 
   exec {'compare_audit': 
     command      => "$script_compare",
-    #path        => '/home/oracle/system/audit',
+    #path        => "/home/oracle/system/audit",
     #path        => "$dir_audit",
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_audit",
     refreshonly => true,
@@ -53,7 +53,7 @@ class oraaud::install (
   #exec {"install_ora_audit.sh ${oraaud_stdin}":
   exec {'install_audit':
     command     => "$script_audit",
-    #path        => '/home/oracle/system/audit',
+    #path        => "/home/oracle/system/audit",
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_audit",
     refreshonly => true,
     notify      => Exec['cycledb'],
@@ -63,7 +63,7 @@ class oraaud::install (
     command     => 'sh -c for DB_NAME in $(/home/oracle/system/usfs_local_sids | sed \'s/[0-9]$//\'); do export DB_NAME; echo srvctl stop database -d $DB_NAME -o immediate; srvctl stop database -d $DB_NAME -o immediate; echo srvctl start database -d $DB_NAME; srvctl start database -d $DB_NAME; done',
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
     refreshonly => true,
-    user        => $db_user,
+    user        => "$db_user",
     notify      => Exec['marker_rm'],
   }
 
@@ -71,7 +71,7 @@ class oraaud::install (
     command     => 'find /opt/oracle/admin/*/adump -name "*aud" -mtime +2 | xargs rm',
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
     refreshonly => true,
-    user        => $db_user,
+    user        => "$db_user",
     notify      => Exec['marker_touch'],
   }
 
@@ -79,12 +79,12 @@ class oraaud::install (
     command     => 'touch /home/oracle/system/audit/.audit_marker_late.txt /home/oracle/system/audit/.audit_marker_newer_pending.txt /home/oracle/system/audit/.audit_marker_newer.txt',
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
     refreshonly => true,
-    user        => $db_user,
+    user        => "$db_user",
     notify      => Exec['service_config'],
   }
 
   exec {'service_config':
-    command     => '/home/oracle/system/audit/config_oraaud_OS_service.sh',
+    command     => "/home/oracle/system/audit/config_oraaud_OS_service.sh",
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
     refreshonly => true,
   }
