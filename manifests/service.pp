0edc8f286b5cad04a294397b3f2a0f5eb0e44482
Ron Compos <rcompos@gmail.com>
2016-08-11 16:49:00 -0600

    Disable refreshonly.

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,7 +4,7 @@ class oraaud::service (
   exec {'service_config':
     command     => "/home/oracle/system/audit/config_oraaud_OS_service.sh",
     path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
-    refreshonly => true,
+    #refreshonly => true,
     notify      => Exec['cycle_db'],
   }
 
