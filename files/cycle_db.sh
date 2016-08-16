9f76d8f24914cd8d96fc2053d17ad902432af9b5
Ron Compos <rcompos@gmail.com>
2016-08-16 16:39:45 -0600

    Append output to log file.

--- files/cycle_db.sh
+++ files/cycle_db.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-for DB_NAME in $(/tmp/usfs_local_sids | sed \'s/[0-9]$//\'); do export DB_NAME; echo srvctl stop database -d $DB_NAME -o immediate; srvctl stop database -d $DB_NAME -o immediate; echo srvctl start database -d $DB_NAME; srvctl start database -d $DB_NAME; done
+set -x
+for DB_NAME in $(/tmp/usfs_local_sids | sed 's/[0-9]$//'); do
+  export DB_NAME
+  echo srvctl stop database -d $DB_NAME -o immediate
+  srvctl stop database -d $DB_NAME -o immediate
+  echo srvctl start database -d $DB_NAME
+  srvctl start database -d $DB_NAME
+done 2>&1 | tee -a /home/oracle/system/audit/.audit/install_ora_audit.sh
