b244c466c58b2962fc1ce80a746bbc7dc38cc4bd
Ron Compos <rcompos@gmail.com>
2016-08-16 14:56:56 -0600

    refreshonly false.

--- manifests/service.pp
+++ manifests/service.pp
@@ -12,7 +12,7 @@ class oraaud::service (
     #command     => "/bin/sh -c for DB_NAME in $(/home/oracle/system/usfs_local_sids | sed \'s/[0-9]$//\'); do export DB_NAME; echo srvctl stop database -d $DB_NAME -o immediate; srvctl stop database -d $DB_NAME -o immediate; echo srvctl start database -d $DB_NAME; srvctl start database -d $DB_NAME; done",
     command     => "$dir_audit/$script_cycle_db",
     path        => "$path_default:$dir_audit",
-    refreshonly => true,
+    refreshonly => false,
     user        => "$db_user",
     notify      => Exec['marker_rm'],
     #subscribe   => File["$dir_audit/$script_cycle_db"],
