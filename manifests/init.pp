887e9a1789497344152dbe029fc4cc0a5cf7ae1d
Bryan Belanger <bbelanger@azcender.com>
2016-08-25 14:49:43 -0400

    Reworked classes and gems

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,19 @@
 # == Class: oraaud
 class oraaud (
-  String $dir_src          = ::oraaud::params::dir_src,
-  String $dir_audit        = ::oraaud::params::dir_audit,
-  String $script_audit     = ::oraaud::params::script_audit,
-  String $script_compare   = ::oraaud::params::script_compare,
-  String $script_cycle_db  = ::oraaud::params::script_cycle_db,
-  String $script_marker_rm = ::oraaud::params::script_marker_rm,
-  String $file_tar         = ::oraaud::params::file_tar,
-  String $path_default     = ::oraaud::params::path_default,
-  String $expect_package   = ::oraaud::params::expect_package,
-  String $expect_ensure    = ::oraaud::params::expect_ensure,
-  String $db_user          = ::oraaud::params::db_user,
-  String $db_group         = ::oraaud::params::db_group,
-  Optional[String] $scp_pw = ::oraaud::params::scp_pw,
-
-) inherits ::oraaud::params {
+  String $dir_src,
+  String $dir_audit,
+  String $script_audit,
+  String $script_compare,
+  String $script_cycle_db,
+  String $script_marker_rm,
+  String $file_tar,
+  String $path_default,
+  String $expect_package,
+  String $expect_ensure,
+  String $db_user,
+  String $db_group,
+  Optional[String] $scp_pw,
+) {
 
   anchor { 'oraaud::begin': }     ->
   class  { '::oraaud::prereq': }  ->
--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe 'oraaud' do
+  let(:params) {
+    {
+      dir_src:          '/fslink/sysinfra/oracle/scripts/misc/auditing',
+      dir_audit:        '/home/oracle/system/audit',
+      script_audit:     'install_ora_audit.sh',
+      script_compare:   'version_compare.sh',
+      script_cycle_db:  'cycle_db.sh',
+      script_marker_rm: 'marker_rm.sh',
+      file_tar:         'rn-ora_audit_nitc.tar',
+      path_default:     '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
+      expect_package:   'expect',
+      expect_ensure:    'present',
+      db_user:          'oracle',
+      db_group:         'oinstall'
+    }
+  }
+
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
