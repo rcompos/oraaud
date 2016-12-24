2cee52b3252dec364b35f3eed6c4e1a76ab773d2
Jim McBride <jtmfam@gmail.com>
2016-12-23 17:10:23 -0700

    Commented out staging::deploy

--- manifests/prereq.pp
+++ manifests/prereq.pp
@@ -17,12 +17,27 @@ class oraaud::prereq {
     #unless => "ls $dir_audit/.audit_marker_late.txt",
   }
 
+  #TODO notify{"This is a notice: command     => 'tar -pxvf ${::oraaud::dir_src}/${::oraaud::file_tar}', ":}
+  exec { 'fs615_untar':
+    #TODO OLD  command     => '/bin/tar -pxvf /opt/staging/oraaud/rn-ora_audit_nitc.tar',
+    #TODO OLD command     => '/bin/tar -pxvf /fslink/sysinfra/oracle/scripts/misc/auditing/install_ora_audit.sh',
+    command     => '/bin/tar -pxvf ${::oraaud::dir_src}/${::oraaud::file_tar}',
+    cwd         => '/',
+    user        => 'root',
+    #OLD require     => File["/home/oracle/system/audit/"],
+    creates     => File["/home/oracle/system/audit/"],
+    refreshonly => true,
+  }
+
+  # END JIM
+
   file { "${::oraaud::dir_audit}/${::oraaud::script_cycle_db}":
     ensure => file,
     source => "puppet:///modules/oraaud/${::oraaud::script_cycle_db}",
     mode   => 'ug+x',
     owner  => $::oraaud::db_user,
     group  => $::oraaud::db_group,
+    require => Exec['fs615_untar'],   #TODO, new
     #before => Exec['cycle_db'],
   }
 
@@ -40,10 +55,11 @@ class oraaud::prereq {
     before => File["${::oraaud::dir_audit}/${::oraaud::script_audit}"],
   }
 
-  file {"${::oraaud::dir_audit}/${::oraaud::script_audit}":
-    mode   => '0755',
-    #before => Exec['install_audit'],
-  }
+# JTM
+# file {"${::oraaud::dir_audit}/${::oraaud::script_audit}":
+#   mode   => '0755',
+#   #before => Exec['install_audit'],
+# }
 
 
   #file {"$dir_audit/$script_compare":
