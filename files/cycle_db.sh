9df0840718f1a7b31263c10c6a16e8c8320e8d19
Jim McBride <jtmfam@gmail.com>
2016-12-28 08:53:18 -0700

    Removed redundant order of execution; Had cycle_db.sh write to the common log.

--- files/cycle_db.sh
+++ files/cycle_db.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env ksh
+
+export Script_name=$(basename $0)
 touch /home/oracle/system/audit/.audit/install_ora_audit.sh || exit 1
-export LOG=/home/oracle/system/audit/.audit/install_ora_audit.sh
+export LOG=/home/oracle/system/audit/.audit/install_ora_audit.sh.log
 
 {
+   function ECHO {
+      echo -e "$(date "+%Y-%m-%d:%H:%M:%S")-$Script_name: \c" >> $LOG
+      echo "$*"
+   }
+
    alias shopt=': '; UID=$(id | sed 's|(.*||;s|.*=||'); . /home/oracle/.bash_profile
    for ORACLE_SID in $(/fslink/sysinfra/oracle/common/db/usfs_local_sids); do
+      ECHO "$(date) ========================================================================"
       export ORACLE_SID;
-      echo "$(date): ORACLE_SID=$ORACLE_SID";
-      echo "shutdown immediate" | sqlplus -s / as sysdba;
-      echo "shutdown abort" | sqlplus -s / as sysdba;
-      echo "Startup:";
-      echo "startup" | sqlplus -s / as sysdba;
-      echo
-      echo "========================================================================"
+      ECHO "ORACLE_SID=$ORACLE_SID";
+      ECHO "shutdown immediate" | sqlplus -s / as sysdba;
+      ECHO "shutdown abort" | sqlplus -s / as sysdba;
+      ECHO "Startup:";
+      ECHO "startup" | sqlplus -s / as sysdba;
+      ECHO
    done
-} 2>&1 | tee -a /home/oracle/system/audit/.audit/install_ora_audit.sh
+} 2>&1 | tee -a $LOG
--- manifests/prereq.pp
+++ manifests/prereq.pp
@@ -4,12 +4,14 @@ class oraaud::prereq {
     ensure => $::oraaud::expect_ensure,
     name   => $::oraaud::expect_package,
   }
-  -> package{ "${::oraaud::file_rpm_noext}":
+
+  package{ "${::oraaud::file_rpm_noext}":
      provider => 'rpm',
      ensure   => 'present',
      source   => "${::oraaud::dir_src}/${::oraaud::file_rpm}",
   }
-  -> file { "${::oraaud::dir_audit}/${::oraaud::script_marker_rm}":
+
+  file { "${::oraaud::dir_audit}/${::oraaud::script_marker_rm}":
     ensure => file,
     source => "puppet:///modules/oraaud/${::oraaud::script_marker_rm}",
     mode   => 'ug+x',
@@ -17,19 +19,22 @@ class oraaud::prereq {
     group  => $::oraaud::db_group,
     #before => Exec['marker_rm'],
   }
-  -> file { [ "$::oraaud::dir_audit", "$::oraaud::dir_audit/.audit" ] :
+
+  file { [ "$::oraaud::dir_audit", "$::oraaud::dir_audit/.audit" ] :
     ensure => 'directory',
     owner  => 'oracle',
     group  => 'oinstall',
     mode   => '0755',
     before => File["${::oraaud::dir_audit}/${::oraaud::script_audit}"],
   }
-  -> file {"${::oraaud::dir_audit}/${::oraaud::script_audit}":
+
+  file {"${::oraaud::dir_audit}/${::oraaud::script_audit}":
     mode   => '0755',
     before =>  Exec['cycle_db'],
     require => Package["${::oraaud::file_rpm_noext}"],
   } 
-  -> file { "${::oraaud::dir_audit}/${::oraaud::script_cycle_db}":
+
+  file { "${::oraaud::dir_audit}/${::oraaud::script_cycle_db}":
     ensure => file,
     source => "puppet:///modules/oraaud/${::oraaud::script_cycle_db}",
     mode   => 'ug+x',
